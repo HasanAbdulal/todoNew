04f990e3f929f6c0973f1f5e893222132c6065fb
JAVA_Basics <hasan.abdulal.eu@gmail.com>
2023-08-30 18:12:34 +0200

    The first project Java

--- src/NewKlass.java
+++ src/NewKlass.java
@@ -14,7 +14,7 @@ public class NewKlass {
     nextBigDecimal();
     nextPattern();*/
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         // Ask the user to enter username
         /*Scanner scanner = new Scanner(System.in);
         System.out.print("Entrez votre nom : ");
@@ -22,7 +22,7 @@ public class NewKlass {
         System.out.println(nom);*/
 
         // Ask the user to enter two numbers and display their sum
-        float nb1, nb2;
+        float nb1, nb2, sum;
         Scanner nbOne = new Scanner(System.in);
 
         System.out.print("Enter the first number: ");
@@ -30,11 +30,13 @@ public class NewKlass {
 
         System.out.print("Enter the second number: ");
         nb2 = nbOne.nextFloat();
+
         // Without using the () for the sum, the result is wrong. It will concatenate the two numbers instead of adding them.
         // System.out.println("The sum of the two numbers is: " + nb1 + nb2);
         // The Second way to do it is to use the sum, like this : sum = nb1 + nb2; And put the sum in the println
         // System.out.println("The sum of the two numbers is: " + sum);
-        System.out.println("The sum of the two numbers is: " + (nb1 + nb2));
+        sum = nb1 + nb2;
+        System.out.println("The sum of the two numbers is: " + sum);
 
         // Testing the charAt() method with a string input by user
        /* Scanner in = new Scanner(System.in);
