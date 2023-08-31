d8b902946245cd684f1cad614003064fd3f5acf8
JAVA_Basics <hasan.abdulal.eu@gmail.com>
2023-08-31 23:15:18 +0200

    The first project Java : Outer, Inner or Nested loops

--- src/NestedLoop.java
+++ src/NestedLoop.java
@@ -1,11 +1,8 @@
 public class NestedLoop {
     public static void main(String[] args) {
         // Nested Loop
-<<<<<<< HEAD
-        /*for (int i = 1; i < 4; i++) { // Outer Loop
-=======
+
         for (int i = 1; i < 4; i++) { // Outer Loop
->>>>>>> origin/master
             System.out.println("Outer Loop: " + " i = " + i);
             for (int j = 1; j < 4; j++) { // Inner Loop or Nested Loop
                 System.out.println("Inner Loop: " + " i = " + i + " ,j = " + j);
@@ -13,8 +10,8 @@ public class NestedLoop {
                     System.out.println("Nested Loop: " + " i = " + i + " ,j = " + j + " ,k = " + k + "\n");
                 }
             }
-<<<<<<< HEAD
-        }*/
+
+        }
         for (int i = 1; i < 4; i++) { // Outer Loop
             System.out.println("Outer Loop: " + " i = " + i);
             for (int j = 1; j < 4; j++) { // Inner Loop or Nested Loop
@@ -23,8 +20,7 @@ public class NestedLoop {
             for (int k = 1; k < 4; k++) {
                 System.out.println("Nested Loop: " + " i = " + i + " ,k = " + k + "\n");
             }
-=======
->>>>>>> origin/master
+
         }
     }
 }
